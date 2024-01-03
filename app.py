206efdab78305de90cd9ad90423342a7e5dcd0d2
cheesege <cheese5skin@gmail.com>
2024-01-04 02:07:24 +0800

    add download add submit add pytube in app.py add dl.css remake css import

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from pytube import YouTube as YT
 app = Flask(__name__)
 #是固定用法，以便讓 Flask 知道在哪裡尋找資源。
 
@@ -11,6 +13,18 @@ def index():
 @app.route('/tool')
 def tool():
     return render_template('tool.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        text_input = request.form['text_input']
+        ytb = YT(text_input)
+        videos = ytb.streams.filter(only_audio=True).first()
+        url = videos.url
+        fn = videos.title
+        pic = ytb.thumbnail_url
+        print(url)
+        return render_template('download.html',link = url , name = fn , png = pic)
 
 if __name__ == '__main__':
     app.run(debug=True)
